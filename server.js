636002aa589753cdc4899ac5e11c0742947182c9
masternpolo <masternpolo@gmail.com>
2025-04-19 23:06:11 +0100

    updating cover img upload

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const AppError = require('./utils/AppError');
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
+app.use('/public',express.static('public'));
 app.set('view engine', 'ejs');
 app.use('/', userRoutes);
 app.use('/', bookRoutes);
