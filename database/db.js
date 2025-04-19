f7fafe02031a9893870254fe68dafae4a0ec9481
masternpolo <masternpolo@gmail.com>
2025-04-19 21:45:07 +0100

    updating env file

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DB_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  ssl:{ rejectUnauthorized: false },
 });
 
 // Optional: test connection
