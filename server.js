13989a10ea0b80d5a669e4890bb24ff7b34dca1f
masternpolo <masternpolo@gmail.com>
2025-04-20 11:25:51 +0100

    rectifying image not showing issues

--- server.js
+++ server.js
@@ -17,17 +17,13 @@ const AppError = require('./utils/AppError');
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/public',express.static('public'));
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use('/', userRoutes);
 app.use('/', bookRoutes);
 
 
 const io = socketHandler(server);
-
-// const path = require('path');
-app.use(express.static('public'));
-
 
 
 app.all('*', (req, res, next) => {
